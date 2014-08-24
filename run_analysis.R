aead2166d72cb1d4bed749ed9f2e76fb26f1a654
Oscar <oscar.pdr@gmail.com>
2014-08-24 15:48:45 +0200

    Updated code for solving all 5 steps

--- run_analysis.R
+++ run_analysis.R
@@ -60,3 +60,8 @@ all_data <- cbind(x_data, y_data, subject_data)
 # Create a second, independent tidy data set with the average of each variable
 # for each activity and each subject
 ###############################################################################
+
+# 66 <- 68 columns but last two (activity & subject)
+averages_data <- ddply(all_data, .(subject, activity), function(x) colMeans(x[, 1:66]))
+
+write.table(averages_data, "averages_data.txt", row.name=FALSE)
