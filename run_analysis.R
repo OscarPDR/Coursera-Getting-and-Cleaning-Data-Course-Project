fa0ba4c7cbdf3af7805979ebba7ea5782f7c96c9
Oscar <oscar.pdr@gmail.com>
2014-08-24 12:36:34 +0200

    Steps 1 and 2 completed

---
+++ run_analysis.R
@@ -0,0 +1,28 @@
+# Step 1
+# Merge the training and test sets to create one data set
+
+x_train <- read.table("train/X_train.txt")
+y_train <- read.table("train/y_train.txt")
+subject_train <- read.table("train/subject_train.txt")
+
+train_data <- rbind(x_train, y_train)
+train_data <- rbind(train_data, subject_train)
+
+x_test <- read.table("test/X_test.txt")
+y_test <- read.table("test/y_test.txt")
+subject_test <- read.table("test/subject_test")
+
+test_data <- rbind(x_test, y_test)
+test_data <- rbind(test_data, subject_test)
+
+x_data <- rbind(x_train, x_test)
+
+all_data <- rbind(train_data, test_data)
+
+# Step 2
+# Extract only the measurements on the mean and standard deviation for each measurement
+
+features <- read.table("features.txt")
+mean_and_std_features <- grep("-(mean|std)\\(\\)", features[, 2])
+x_mean_and_std_data <- x_data[, mean_and_std_features]
+names(x_mean_and_std_data) <- features[mean_and_std_features, 2]
